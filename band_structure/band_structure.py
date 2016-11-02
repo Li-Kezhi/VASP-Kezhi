4f3a265822109d06ae185f874aa01c55d4a33ddc
Li-Kezhi <daliwa7@sina.com>
2016-11-02 23:56:21 +0800

    Update format

--- band_structure/band_structure.py
+++ band_structure/band_structure.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
 '''
 Plot band structure
 
@@ -20,7 +19,10 @@ def count():
     while True:
         yield i
         i += 1
+
+
 indexLine = count()
+
 
 def distance(point1, point2):
     '''
@@ -28,7 +30,8 @@ def distance(point1, point2):
     Input: two points as form of list
     Example: point1 = [0.0, 0.5, 0.0]
     '''
-    return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2 + (point1[2] - point2[2])**2)
+    return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2 +
+        (point1[2] - point2[2])**2)
 
 # Read KPOINTS to get critical k-points
 file = 'KPOINTS'
@@ -37,12 +40,16 @@ isKpointsBeginning = False
 with open(file, 'r') as f:
     for line in f:
         splitting = [item for item in line.split() if item != '']
-        if splitting != [] and splitting[0][0] in 'Rr':    # find reciprocal/Reciprocal
+        if splitting != [] and splitting[0][0] in 'Rr':  # find R(r)eciprocal
             isKpointsBeginning = True
             continue
         if isKpointsBeginning == True:
             if splitting != []:
-                kpoint = [float(splitting[0]), float(splitting[1]), float(splitting[2])]
+                kpoint = [
+                    float(splitting[0]), 
+                    float(splitting[1]), 
+                    float(splitting[2])
+                ]
                 if not kpoint in kpoints:
                     kpoints.append(kpoint)
 
@@ -57,7 +64,7 @@ bandOccupancy = {}
 kpointDistance = []
 with open(file, 'r') as f:
     for line in f:
-        if lineNum == 5:    # header
+        if lineNum == 5:  # header
             bandNum = int(line.split()[2])
             kpointNum = int(line.split()[1])
             # Initiate bandEnergy and bandOccupancy
@@ -67,9 +74,13 @@ with open(file, 'r') as f:
 
         if lineNum >= 7:
             currentIndexLine = indexLine.next()
-            if currentIndexLine % (bandNum + 2) == 0:    # header
+            if currentIndexLine % (bandNum + 2) == 0:  # header
                 splitting = [item for item in line.split() if item != '']
-                kpoint = [float(splitting[0]), float(splitting[1]), float(splitting[2])]
+                kpoint = [
+                    float(splitting[0]), 
+                    float(splitting[1]), 
+                    float(splitting[2])
+                ]
                 if kpoint in kpoints:
                     kpointMark.append(currentIndexLine / (bandNum + 2))
                 # Calculate respiracal distance
@@ -81,8 +92,9 @@ with open(file, 'r') as f:
                     lastKpoint = kpoint
             elif currentIndexLine % (bandNum + 2) < (bandNum + 1):
                 splitting = [item for item in line.split() if item != '']
-                bandEnergy[int(splitting[0]) - 1].append(float(splitting[1]))
-                bandOccupancy[int(splitting[0]) - 1].append(float(splitting[2]))
+                splitting0 = int(splitting[0])
+                bandEnergy[splitting0 - 1].append(float(splitting[1]))
+                bandOccupancy[splitting0 - 1].append(float(splitting[2]))
 
         lineNum += 1
 
@@ -96,11 +108,11 @@ for i in xrange(kpointNum):
 for i in xrange(bandNum):
     plt.plot(X, bandEnergy[i])
 
-Y = np.zeros_like(X)    # add Fermi reference line
+Y = np.zeros_like(X)  # add Fermi reference line
 plt.plot(X, Y)
 
-for kpoint in kpointMark:    # add critical k-points line
-    plt.axvline(x = X[kpoint], color = 'black', linestyle = 'dotted')
+for kpoint in kpointMark:  # add critical k-points line
+    plt.axvline(x=X[kpoint], color='black', linestyle='dotted')
 
 plt.ylabel('E - Ef (eV)')
 
--- split_dos/split_dos.py
+++ split_dos/split_dos.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
 '''
 Split DOSCAR into seperate files
 
@@ -11,10 +10,10 @@ __author__ = "LI Kezhi"
 __date__ = "$2016-10-23$"
 __version__ = "1.1"
 
-rows = 0    # the total number of rows
+rows = 0  # the total number of rows
 
 fileNum = 0
-isNewStart = True    # Flag: beginning of a single file
+isNewStart = True  # Flag: beginning of a single file
 
 i = -1
 def count():
@@ -23,12 +22,14 @@ def count():
     while True:
         i += 1
         yield i
+
+
 index = count()
 
 with open('DOSCAR', 'r') as f:
     lineNum = 0
     for line in f:
-        if lineNum == 5:    # pass the header
+        if lineNum == 5:  # pass the header
             splitting = [item for item in line.split() if item != '']
             rows = int(splitting[2])
             isNewStart = False
@@ -37,12 +38,12 @@ with open('DOSCAR', 'r') as f:
             if isNewStart:
                 file = open('DOS' + str(fileNum), 'w')
                 isNewStart = False
-                continue    # pass the header
+                continue  # pass the header
             file.write(line)
-            if index.next() % rows == 0:    # end of data
+            if index.next() % rows == 0:  # end of data
                 fileNum += 1
                 isNewStart = True
                 file.close()
         lineNum += 1
 
-print ('%d DOS file(s) have been splitted named as DOS0, DOS1...' % fileNum)
+print('%d DOS file(s) have been splitted named as DOS0, DOS1...' % fileNum)
