35e6895a0cebd8e36b3ac6a81a4dfcb2751d3eaf
Li-Kezhi <lkz13@mails.tsinghua.edu.cn>
2017-03-26 15:13:15 +0800

    Solve errenous markings

--- create_input/add_arg.py
+++ create_input/add_arg.py
@@ -6,6 +6,8 @@ output = open('POSCARout', 'w')
 with open('POSCAR', 'r') as f:
     isCrystalSize = False
     isEndOfCrystalSize = False
+    isElementsLine = False
+    isEndOfElemtnsLine = False
     isCoordinate = False
 
     for line in f:
@@ -37,17 +39,25 @@ with open('POSCAR', 'r') as f:
                         isEndOfCrystalSize = True
                         isCrystalSize = True
 
-        # Find the beginning of coordination data
-        if isEndOfCrystalSize is True and len(splitted) >= 3:
-            for i, item in enumerate(splitted):
-                if i == 3:
-                    break
+        # Find the beginning of elements data
+        if isEndOfCrystalSize and not isEndOfElemtnsLine:
+            for item in splitted:
+                preStatus = isElementsLine
                 try:
                     float(item)
                 except ValueError:
+                    isElementsLine = True
                     break
-                if i == 2:
-                    isCoordinate = True
+                finally:
+                    if preStatus: # isElementsLine was set True before
+                        isEndOfElemtnsLine = True
+
+        # Find the beginning of coordination data
+        if isEndOfElemtnsLine:
+            if splitted[0][0] in 'dDkKcC':
+                isCoordinate = True
+                output.write(line)
+                continue
 
         # Actions
         if not isCoordinate:
